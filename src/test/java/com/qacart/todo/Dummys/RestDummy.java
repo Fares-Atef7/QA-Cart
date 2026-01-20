e2359c738a2b869bb6948e2cd88f08bba4f58236
Fares-Atef7 <faresatef21@gmail.com>
2026-01-20 12:03:43 +0200

    RestAssured&SeleniumDummyClass_V2

--- src/test/java/com/qacart/todo/Dummys/RestDummy.java
+++ src/test/java/com/qacart/todo/Dummys/RestDummy.java
@@ -1,28 +1,30 @@
 package com.qacart.todo.Dummys;
 
-import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.given;
+import com.qacart.todo.API.RegisterAPI;
 
 public class RestDummy {
     public static void main(String[] args) {
 
-        String requestBody="{\n" +
-                "    \"email\":\"khfolakh122@example.com\",\n" +
-                "    \"firstName\":\"ahmed\",\n" +
-                "    \"lastName\":\"ashraf\",\n" +
-                "    \"password\":\"123456789\"\n" +
-                "}";
+        RegisterAPI registerAPI=new RegisterAPI();
+        registerAPI.Register();
+        System.out.println(registerAPI.getAccessToken());
+        System.out.println(registerAPI.getUserID());
+        System.out.println(registerAPI.getFirstName());
 
-        Response respone=given()
-                .baseUri("https://qacart-todo.herokuapp.com")
-                .header("Content-Type", "application/json")
-                .body(requestBody)
-        .when()
-                .post("/api/v1/users/register")
-        .then()
-                .extract().response();
-
-        System.out.println(respone.prettyPrint());
     }
 }
+
+/*
+*  User user=new User("test2","test22","khfolakh132353@example.com","Fares A9");
+
+        Response respone = given()
+                .baseUri("https://qacart-todo.herokuapp.com")
+                .header("Content-Type", "application/json")
+                .body(user)
+                .when()
+                .post("/api/v1/users/register")
+                .then()
+                .extract().response();
+        String accessToken=respone.path("access_token");
+
+        System.out.println(accessToken);*/
--- src/test/java/com/qacart/todo/Utilis/ConfigUtils.java
+++ src/test/java/com/qacart/todo/Utilis/ConfigUtils.java
@@ -7,17 +7,17 @@ public class ConfigUtils {
     private static ConfigUtils configUtils;
 
     public ConfigUtils() {
-        String env = System.getProperty("env", "production");
-        switch (env) {
-            case "PRODUCTION":
-                properties = PropertiesUtils.loadProperties("src/test/java/com/qacart/todo/config/production.properties");
-                break;
-            case "LOCAL":
-                properties=PropertiesUtils.loadProperties("src/test/java/com/qacart/todo/config/local.properties");
-                break;
-            default:
-                throw new RuntimeException("invalid input");
-        }
+//        String env = System.getProperty("env", "production");
+//        switch (env) {
+//            case "PRODUCTION":
+//                properties = PropertiesUtils.loadProperties("src/test/java/com/qacart/todo/config/production.properties");
+//                break;
+//            case "LOCAL":
+//                properties=PropertiesUtils.loadProperties("src/test/java/com/qacart/todo/config/local.properties");
+//                break;
+////            default:
+////                throw new RuntimeException("invalid input");
+//        }
     }
 
     public static ConfigUtils getInstance() {
@@ -28,6 +28,8 @@ public class ConfigUtils {
     }
 
     public String getBaseUrl() {
+//        String baseUrl = properties.getProperty("baseUrl");
+        properties = PropertiesUtils.loadProperties("src/test/java/com/qacart/todo/config/production.properties");
         String baseUrl = properties.getProperty("baseUrl");
         if (baseUrl != null) return baseUrl;
         throw new RuntimeException("could not find the Base Url");
